2ccf1377d613aa05b930ae3ff41302783853c1e8
QuinSDev <quindev97@gmail.com>
2024-06-26 14:57:16 -0500

    Se crea un clase de tipo record de Datos Libro que extraera solo la información necesaria del libro de la API para guardarla en la base de datos.

--- src/main/java/com/alura/hojearte/model/DatosLibro.java
+++ src/main/java/com/alura/hojearte/model/DatosLibro.java
@@ -1,2 +1,15 @@
-package com.alura.hojearte.model;public record DatosLibro() {
+package com.alura.hojearte.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors")List<DatosAutor> autor,
+        @JsonAlias("languages")List<String> idiomas,
+        @JsonAlias("dowload_count") Integer numeroDeDescargas
+        ) {
 }
