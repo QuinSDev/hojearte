fe6619c755017d820f389fb16dea2e5d427a05ff
QuinSDev <quindev97@gmail.com>
2024-06-26 08:52:53 -0500

    Se configuran las dependencias necesarias para trabajar en el proyecto y también las propiedades para la conexión con la base de datos.

---
+++ src/main/java/com/alura/hojearte/HojearteApplication.java
@@ -0,0 +1,13 @@
+package com.alura.hojearte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojearteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojearteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/hojearte/HojearteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.hojearte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HojearteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
