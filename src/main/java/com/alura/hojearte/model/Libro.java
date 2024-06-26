fd2aa9d2eeebbaa45ef4d638a7cc9a0673b3df18
QuinSDev <quindev97@gmail.com>
2024-06-26 15:00:08 -0500

    Se crea la clase Libro.

--- src/main/java/com/alura/hojearte/model/Libro.java
+++ src/main/java/com/alura/hojearte/model/Libro.java
@@ -1,2 +1,73 @@
-package com.alura.hojearte.model;public class Libro {
+package com.alura.hojearte.model;
+
+import jakarta.persistence.*;
+
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String titulo;
+    @ManyToOne
+    private Autor autor;
+    @Enumerated(EnumType.STRING)
+    private Idioma idioma;
+
+    private Integer numeroDeDescargas;
+
+    public Libro(DatosLibro datosLibro) {
+        this.titulo = datosLibro.titulo();
+        this.idioma = Idioma.fromString(datosLibro.idiomas().stream()
+                .limit(1)
+                .collect(Collectors.joining()));
+        this.numeroDeDescargas = datosLibro.numeroDeDescargas();
+    }
+
+    public Libro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Idioma getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(Idioma idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Integer numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
 }
