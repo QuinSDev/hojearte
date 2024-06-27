1a4e217275605d7cce00f6533b8f336ed98b735b
QuinSDev <quindev97@gmail.com>
2024-06-26 19:19:51 -0500

    Se crea el repositorio de Autor con algunas consultas hacia la BD.

--- src/main/java/com/alura/hojearte/repository/AutorRepository.java
+++ src/main/java/com/alura/hojearte/repository/AutorRepository.java
@@ -1,2 +1,20 @@
-package com.alura.hojearte.repository;public interface AutorRepository {
+package com.alura.hojearte.repository;
+
+import com.alura.hojearte.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNombre(String nombre);
+
+    @Query("SELECT a FROM Autor a WHERE a.fechaDeNacimiento <=:añoIngresado AND a.fechaDeFallecimiento > :añoIngresado")
+    List<Autor> listarAutoresPorAño(Integer añoIngresado);
+
+    @Query("SELECT a FROM Autor a WHERE a.nombre ILIKE %:nombre%")
+    Optional<Autor> buscarAutorPorNombre(String nombre);
 }
