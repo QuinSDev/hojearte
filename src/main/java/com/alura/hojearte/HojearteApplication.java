8552ac73d569fea4481c0a54b0cfcae7c9782e91
QuinSDev <quindev97@gmail.com>
2024-06-26 19:24:21 -0500

    Se añade la anotación @ComponentScan

--- src/main/java/com/alura/hojearte/HojearteApplication.java
+++ src/main/java/com/alura/hojearte/HojearteApplication.java
@@ -5,17 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class HojearteApplication implements CommandLineRunner {
 
+	@Autowired
+	private Principal principal;
 	public static void main(String[] args) {
 		SpringApplication.run(HojearteApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
 		principal.mostrarElMenu();
 	}
 }
