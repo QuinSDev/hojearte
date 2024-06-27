04e53a0fe02f4a9d0318d5f253e0337edcfbfc3c
QuinSDev <quindev97@gmail.com>
2024-06-26 19:22:19 -0500

    Se crea la clase servicio de Libro con algunos métodos como verificarAutor, verificarLibro, guardarAutor y guardarLibroConAutor.

--- src/main/java/com/alura/hojearte/service/LibroService.java
+++ src/main/java/com/alura/hojearte/service/LibroService.java
@@ -1,2 +1,51 @@
-package com.alura.hojearte.service;public class LibroService {
+package com.alura.hojearte.service;
+
+import com.alura.hojearte.model.Autor;
+import com.alura.hojearte.model.DatosAutor;
+import com.alura.hojearte.model.DatosLibro;
+import com.alura.hojearte.model.Libro;
+import com.alura.hojearte.repository.AutorRepository;
+import com.alura.hojearte.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LibroService {
+
+    @Autowired
+    private LibroRepository libroRepository;
+    @Autowired
+    private AutorRepository autorRepository;
+
+    public Optional<Libro> verificarLibroExistenteEnBD(String titulo) {
+        return libroRepository.findByTitulo(titulo);
+    }
+
+    public Optional<Autor> verificarAutorExistenteEnBD(String nombre) {
+        return autorRepository.buscarAutorPorNombre(nombre);
+    }
+
+    public Autor guardarAutor(DatosLibro datosLibro) {
+        //Asumiremos que el libro solo tiene un autor
+        DatosAutor datosAutor = datosLibro.autor().get(0);
+
+        Optional<Autor> autorExistente = verificarAutorExistenteEnBD(datosAutor.nombre());
+        if (autorExistente.isPresent()) {
+            return autorExistente.get();
+        } else {
+            Autor autor = new Autor(datosAutor);
+            return autorRepository.save(autor);
+        }
+    }
+
+    public Libro guardarLibroConElAutor(DatosLibro datosLibro) {
+        Autor autor = guardarAutor(datosLibro);
+
+        Libro libro = new Libro(datosLibro);
+        libro.setAutor(autor);
+
+        return libroRepository.save(libro);
+    }
 }
